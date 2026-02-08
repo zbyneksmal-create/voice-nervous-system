6a82fc287d721937709c65a40d69fe6bb09a8b77
zbyneksmal-create <zbynek.smal@gmail.com>
2026-02-08 21:11:08 +0100

    add service worker for offline use

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "vns-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./stabilizace.mp3",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((r) => r || fetch(event.request))
+  );
+});
